585e523c0d181973de340e3cbe3ccc4a89aabeae
Seven719 <ilie.andronie@gmail.com>
2023-12-14 10:04:02 +0000

    Set snap pixel to 0
    Set the desired font with the correct font entry

--- dwm/config.h
+++ dwm/config.h
@@ -30,11 +30,11 @@ static void toggleFScreen(const Arg *arg)
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"JetBrains Mono:size=11", "monospace:size=11", "fontawesome:size=11"};
-static const char dmenufont[]       = "JetBrains Mono:size=11";
+static const char *fonts[]          = {"JetBrainsMono Nerd Font:Regular:size=11", "monospace:size=11", "fontawesome:size=11"};
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:Regular:size=11";
 
 //background color
 static const char col_gray1[]       = "#222222";
