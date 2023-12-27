78a61545448f06a81334dc467826d55829658ed7
Seven719 <ilie.andronie@gmail.com>
2023-12-27 13:56:57 +0000

    removed autoindent

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -5,11 +5,6 @@ require'nvim-treesitter.configs'.setup {
     auto_install = true,
 
     autotag = { enable = true},
-    indent = { 
-        enable = true,
-        -- disable = {'c_header'}
-    },
-
     highlight = { 
         enable = true, 
         additional_vim_regex_highlighting = false
