e3c5b0f4e0e3cd3b07ede8b0b997e1f65fe51997
Seven719 <ilie.andronie@gmail.com>
2024-03-19 22:19:21 +0000

    Added support for go

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "lua_ls", "rust_analyzer", "pyright", "tsserver" }
+local servers = { "html", "cssls", "clangd", "lua_ls", "rust_analyzer", "pyright", "tsserver", "gopls"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
