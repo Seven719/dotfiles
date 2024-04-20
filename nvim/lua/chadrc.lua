992f9635d7fafb8fb1245ab4b93eac5d2dc96f7a
Seven719 <73722884+Seven719@users.noreply.github.com>
2024-04-20 20:38:33 +0100

    Added yew support
    Changed colorscheme

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.ui = {
-	theme = "kanagawa",
+	theme = "rosepine",
     transparency = true,
 
     statusline = {
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,6 +8,13 @@ return {
 
     {
         "neovim/nvim-lspconfig",
+        init_options = {
+            userLanguages = {
+                eelixir = "html-eex",
+                eruby = "erb",
+                rust = "html",
+            },
+        },
         config = function()
             require("nvchad.configs.lspconfig").defaults()
             require "configs.lspconfig"
