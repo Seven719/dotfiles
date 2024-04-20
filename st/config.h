8afaa8f3ca241f712406c49719838f2b67006096
Seven719 <73722884+Seven719@users.noreply.github.com>
2024-04-20 20:37:24 +0100

    Changed terminal opacity

--- st/config.h
+++ st/config.h
@@ -8,7 +8,7 @@
 static char *font = "JetBrains Mono:Regular:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
-float alpha = 0.97;
+float alpha = 0.95;
 
 /*
  * What program is execed by st depends of these precedence rules:
