48262b028524cbdf4f75c2f3007f864b8aab8794
Seven719 <73722884+Seven719@users.noreply.github.com>
2024-04-30 14:07:15 +0100

    fixed undefined global 'vim' error from lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,3 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
@@ -6,11 +5,22 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "clangd", "lua_ls", "rust_analyzer", "pyright", "tsserver", "gopls"}
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+    }
 end
+
+lspconfig.lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    'vim'
+                }
+            }
+        }
+    }
+}
