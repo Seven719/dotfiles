5dc281db3a2c43df628f6fe3dabbb4461c0d6721
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-08-06 23:24:31 +0100

    Modified brightness and gamma in gammastep

--- nixos/services/user/gammastep.nix
+++ nixos/services/user/gammastep.nix
@@ -5,4 +5,10 @@
     provider = "manual";
     latitude = 51.65;
     longitude = 0.42;
+    settings = {
+        general = {
+            brightness-night = "0.6";
+            gamma-night = "0.8";
+        };
+    };
 }
