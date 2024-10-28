7daea47e97e52688e3f303391969329f4be10194
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-10-28 11:53:57 +0000

    Delete nixos directory
    Switch from nixOS back to Arch

--- nixos/programs/btop.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-    enable = true;
-    settings = {
-        vim_keys = true;
-    };
-}
--- nixos/services/root/openssh.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-    services.openssh.enable = true;
-
-    users.users."${config.user.name}".openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpACQMbFkNKNQIqN+Mnkx1uIlguowd+98GeKIuehKal ilie.andronie@gmail.com"
-    ];
-}
